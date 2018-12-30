8462521975b4f26dab70421a45c919ee9fdeeceb
pepe454 <17fryerd@gmail.com>
2018-12-30 13:41:09 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='linearalgebracalculator',
+    name='Linear-Algebra',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
