020a8f9439c2e5e136a90d00ba0d2ebd7ae3d5ac
pepe454 <17fryerd@gmail.com>
2018-12-30 14:34:12 -0500

    "update directory structure"

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='Linear-Algebra',
+    name='Matrix_Calculator',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
