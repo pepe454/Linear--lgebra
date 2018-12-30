c3f89197f48cd046c8cb7fc0cef60c74a9321ee2
pepe454 <17fryerd@gmail.com>
2018-12-30 12:48:10 -0500

    flask webserver

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username':'Danny'}
+    return render_template('index.html', title="What's up Man!", user=user)
