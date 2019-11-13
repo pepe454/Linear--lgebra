b89875f75a8e3e86b69b8d2b481b1dce8ddfce2b
pepe454 <17fryerd@gmail.com>
2019-11-12 19:48:55 -0500

    add magic methods

--- matrixcalculator.py
+++ matrixcalculator.py
@@ -18,21 +18,26 @@ class Matrix:
     def __init__(self, A, b = None):
         #basic qualities of the matrix
         #let A be a list of lists. This is the matrix
-        #let b be the solution if it is given
+        len0 = len(A[0])
+        for i in A:
+            if len(i) != len0:
+                raise Exception("Invalid matrix entry")
         self.A = A
+
+        #let b be the solution if it is given
         self.b = None
-        if b and len(b) == len(self.A):
+        if b and len(b) == self.rows():
             self.b = b 
         self.RREF = False
-        self.transpose = None
+        self.trans = None
         
         #IMT properties
         self.square = False
         self.inv = None
         self.det = None
         #set the inverse to be the identity matrix at first
-        if len(self.A) == len(self.A[0]):
-            self.inv = [[1 if j == i else 0 for j in range(len(self))] for i in range(len(self))]
+        if self.rows() == self.cols():
+            self.inv = [[1 if j == i else 0 for j in range(self.rows())] for i in range(self.rows())]
             self.det = 1
             self.square = True
             
@@ -51,9 +56,9 @@ class Matrix:
     #matrix operations  
     #first and second are the indices to swap  
     def swap(self, first, second):   
-        if 0 <= first < len(self) and \
-           0 <= second < len(self):
-            self.A[first], self.A[second] = self.A[second], self.A[first]
+        if 0 <= first < self.rows() and \
+           0 <= second < self.rows():
+            self[first], self[second] = self[second], self[first]
             if self.square:
                 self.inv[first], self.inv[second] = self.inv[second], self.inv[first]
                 self.det *= -1
@@ -62,8 +67,8 @@ class Matrix:
     
     #scale 1 row by constant c
     def scale(self, row, c):
-        if 0 <= row < len(self.A):
-            self.A[row] = [c*i if i != 0 else 0.0 for i in self.A[row]]
+        if 0 <= row < self.rows():
+            self[row] = [c*i if i != 0 else 0.0 for i in self[row]]
             if self.square:
                 self.inv[row] = [c*i if i != 0 else 0.0 for i in self.inv[row]] 
                 self.det *= 1/c       
@@ -73,10 +78,10 @@ class Matrix:
     #c is a constant that will scale the first row before it's added
     #second will be added to
     def addtwo(self, first, second, c=1):
-        if 0 <= first < len(self.A) and \
-           0 <= second < len(self.A):
-            temp = [c*i for i in self.A[first]]
-            self.A[second] = list(map(lambda a,b: a + b, temp, self.A[second]))
+        if 0 <= first < self.rows() and \
+           0 <= second < self.rows():
+            temp = [c*i for i in self[first]]
+            self[second] = list(map(lambda a,b: a + b, temp, self[second]))
             if self.square:
                 temp = [c*i for i in self.inv[first]]
                 self.inv[second] = list(map(lambda a,b: a + b, temp, self.inv[second]))
@@ -88,58 +93,64 @@ class Matrix:
         if self.RREF:
             return  
         detailed = ""
-        smaller = min(len(self.A[0]), len(self.A))
+        smaller = min(self.rows(), self.cols())
 
         for step in range(smaller):
             if verbose:
                 detailed += f"Step: {step}\n{str(self)}\n"
             #need a non-zero value in [step][step]
-            if self.A[step][step] == 0:
-                for y in range(step + 1, len(self)):
-                    if self.A[y][step] != 0:
+            if self[step][step] == 0:
+                for y in range(step + 1, self.rows()):
+                    if self[y][step] != 0:
                         self.swap(step, y)
                         break
             
-            if self.A[step][step] != 0:
-                self.scale(step, 1/self.A[step][step])
+            if self[step][step] != 0:
+                self.scale(step, 1/self[step][step])
                 #add rows such that all [step + 1 ... len(A)][step] = 0
-                for y in range(step + 1, len(self)):
-                    const = -1 * (self.A[y][step]/self.A[step][step])
+                for y in range(step + 1, self.rows()):
+                    const = -1 * (self[y][step]/self[step][step])
                     self.addtwo(step, y, const)
                 #add rows above [0 ... step - 1][step] = 0
                 for y in range(step):
-                    if self.A[y][step] != 0:
-                        const = -1 * (self.A[y][step]/self.A[step][step])
+                    if self[y][step] != 0:
+                        const = -1 * (self[y][step]/self[step][step])
                         self.addtwo(step, y, const)    
 
         self.RREF = True 
         if verbose:  
             detailed += f"Step: {str(smaller)}\n{str(self)}\n"
             detailed += "Finished!"
+        if self.det:
+            #multiply along the diagonal
+            for i in range(self.rows()):
+                self.det *= self[i][i]
         return detailed  
 
     #return the solution of the system if b is provided
-    def solution(self):
+    #by default, 
+    def solution(self, fract=True):
         if self.b is not None:
             if not self.RREF:
                 self.gaussianelimination()
                 
             #if any row is all 0's, the system is inconsistent
-            for k in self.A:
+            for k in self:
                 if not any(k):
                     return "The system is inconsitent"
             
+            conv = lambda x: flt2frac(x) if fract else x
             #now return the solution if the system is consistent
             if self.square:
-                return "\n".join([f"x{i} = {flt2frac(x)}" for i, x in enumerate(self.b)])
+                return "\n".join([f"x{i+1} = {conv(x)}" for i, x in enumerate(self.b)]) + "\n"
             else: 
                 #if M < N, there are free variables. 
                 #One free variable for each extra col 
-                first = len(self.A[0]) - len(self.A)            
+                first = self.cols() - self.rows()            
                 sol = "The solution in terms of free variables\n"
                 for i,l in enumerate(self):
-                    tmp = f"x{i+1} = {flt2frac(self.b[i])} + "
-                    tmp += ' + '.join([f"{flt2frac(-w)}x{z+first}" for z,w in enumerate(l[first:])])
+                    tmp = f"x{i+1} = {conv(self.b[i])} + "
+                    tmp += ' + '.join([f"{conv(-w)}x{z+first}" for z,w in enumerate(l[first-1:])])
                     sol += f"{tmp}\n"
                 return sol
         return "No solution was provided"
@@ -149,70 +160,69 @@ class Matrix:
         if self.square: 
             if not self.RREF:
                 self.gaussianelimination()
-            #multiply along the diagonal
-            for i in range(len(self)):
-                self.det *= self.A[i][i]
             return self.det
         #matrix is not square
         return 
                  
-    #retruns the determinant
+    #returns the inverse
     def inverse(self):
         if self.square:
-            k = self.get_det()
-            if k != 0 and k is not None:
-                for j in range(len(self.inv)):
-                    g = "["
-                    for k in range(len(self.inv[0])):
-                        g += str(Fraction(self.inv[j][k]).limit_denominator())
-                        g += ", "
-                    g = g[:-2]
-                    g += "]"
-                    print(g)
+            determ = self.determinant() #will do GE if not in RREF
+            if determ:
+                self.inv = Matrix(self.inv)
                 return self.inv
-            else: 
-                #determinant is zero - the inverse DNE
-                return 
-        else:
-            #matrix isn't square
-            return None
+        return 
 
-    '''
     def transpose(self):
-        self.transposed = not self.transposed
-        if self.square:
-            k = len(self)
-            for i in range(k):
-                for j in range(i, k):
-                    self.A[i][j], self.A[j][i] = self.A[j][i], self.A[i][j]
-            print(self)
-        else:
-            transpose = [[0 for j in range(len(self))] for i in range(len(self.A[0]))]
-            for k in range(len(self.A[0])):
-                for j in range(len(self)):
-                    transpose[k][j] = self.A[j][k]
-            self.A = transpose
-            self.RREF = False
-            print(self)               
-    '''
+        result = [[0 for j in range(self.rows())] for i in range(self.cols())]
+        for i in range(self.cols()):
+            for j in range(self.rows()):
+                result[i][j] = self[j][i]
+        self.trans = Matrix(result) 
+        return self.trans            
     
-    """
     def __add__(self,other):
-        if len(self) == len(other) and len(self.A[0]) == len(other.A[0]):
-            for k in range(len(other)):
-                g = self.dot_operation(self.A[k],other.A[k],lambda x,y: x+y)
-                print(g)
-                self.A[k] = g
-        else:
-            print("undefined")
+        if self.rows() == other.rows() and \
+           self.cols() == other.cols():
+            result = [list(map(lambda x,y: x+y, row, other[ind])) for ind,row in enumerate(self)]
+            return Matrix(result)
+
+    def __sub__(self,other):
+        if self.rows() == other.rows() and \
+           self.cols() == other.cols():
+            result = [list(map(lambda x,y: x-y, row, other[ind])) for ind,row in enumerate(self)]
+            return Matrix(result)
     
     def __mul__(self,other):
-        if len(self[0]) == len(other):
-            
-            
-        else:
-            print("undefined")
-    """       
+        if self.rows() == other.cols() and \
+           self.cols() == other.rows():
+            #resulting matrix is M1 X M2
+            result = [[] for j in other.cols()]
+            return Matrix(result)
+        return
+
+    def __eq__(self, other):
+        if self.rows() != other.rows() or \
+           self.cols() != other.cols():
+            return False
+
+        for i in range(self.rows()):
+            for j in range(self.cols()):
+                if self[i][j] != other[i][j]:
+                    return False
+        return True
+
+    def __getitem__(self, value):
+        return self.A[value]
+
+    def __setitem__(self, index, value):
+        self.A[index] = value
+
+    def rows(self):
+        return len(self.A)
+
+    def cols(self):
+        return len(self.A[0])
 
     def __iter__(self):
         for x in self.A:
@@ -222,7 +232,8 @@ class Matrix:
         string_rep = ""
         if self.b is None:
             string_rep = [f"| {' '.join([flt2frac(x) for x in y])} |" for y in self]
-            return "\n".join(string_rep)   
+            nl = '\n'
+            return f"{nl.join(string_rep)}\n"  
         else:
             for i in range(len(self)):
                 string_rep += f"| {' '.join([flt2frac(x) for x in self.A[i]])} |"
@@ -234,13 +245,27 @@ class Matrix:
 
 
 #n = m
-m = Matrix([[3,4,5,6],[7,4,3,2],[12,13,2,6],[5,9,8,7]],[12,13,14,15])
-print(m.solution())
+m1 = Matrix([[3,4,5,6],[7,4,3,2],[12,13,2,6],[5,9,8,7]],[12,13,14,15])
+m1b = Matrix([[4,5,6,7],[10,12,13,14],[2,3,7,8],[6,9,6,9]])
+t1 = m1.transpose()
+#print(t1)
+#print(i1)
+#print(m1.solution())
+print(m1b - m1)
+
 
 #n > m
-m = Matrix([[1,2,3,12,13],[4,3,2,14,15]], [6,7])
-print(m.solution())
+m2 = Matrix([[1,2,3,12,13],[4,3,2,14,15]], [6,7])
+m2b = Matrix([[5,6,7,8,9],[12,15,16,20,28]])
+t2 = m2.transpose()
+print(m2 - m2b)
+#print(t2)
+#print(m2.solution())
 
 #n < m
-m = Matrix([[1,2,3,12,13],[4,3,2,14,15]], [6,7])
-print(m.solution())
+m3 = Matrix([[1,2,13],[4,3,15],[16,13,17],[20,21,28]], [6,7,5,6])
+t3 = m3.transpose()
+#print(t3)
+#print(m3.solution())
+
+print(m3 + m2)
